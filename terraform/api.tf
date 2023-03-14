d9391379a7fa367184a14bc7e4525a0ed9693566
Pedro Castro <castro@castro4source.lan>
2023-03-14 09:34:10 +0000

    Aircall enterview

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,39 +1,39 @@
-#  module "rest_api_resource" {
-#      source                  = "../modules/aws-gateway/api-gateway-private-lambda"
-#      name                    = "${var.name}" 
-#      description             = var.description
-#      policy                  = <<EOF
-#  {
-#      "Version": "2012-10-17",
-#      "Statement": [
-#          {
-#              "Effect": "Deny",
-#              "Principal": "*",
-#              "Action": "execute-api:Invoke",
-#              "Resource": "execute-api:/${var.rest_api_stage_name}/*/*",
-#              "Condition": {
-#                  "StringNotEquals": {
-#                      "aws:sourceVpc": "${var.vpc_id}"
-#                  }
-#              }
-#          },
-#          {
-#              "Effect": "Allow",
-#              "Principal": "*",
-#              "Action": "execute-api:Invoke",
-#              "Resource": "execute-api:/${var.rest_api_stage_name}/*/*"
-#          }
-#      ]
-#  }
-#  EOF
-#      vpc_endpoint_ids        = [data.aws_vpc_endpoint.endpoint.id]
-#      stage_name              = var.rest_api_stage_name
-#      stage_description       = var.stage_description 
-#      path_part               = var.path_part
-#      method                  = var.http_method
-#      authorization           = var.authorization
-#      lambda_arn              = module.lambda_s3.arn
-#      region                  = var.region
-#      account_id              = var.account_id
-#      credentials             = module.role_lambda_s3.arn
-#  }
+ module "rest_api_resource" {
+     source                  = "../modules/aws-gateway/api-gateway-private-lambda"
+     name                    = "${var.name}" 
+     description             = var.description
+     policy                  = <<EOF
+ {
+     "Version": "2012-10-17",
+     "Statement": [
+         {
+             "Effect": "Deny",
+             "Principal": "*",
+             "Action": "execute-api:Invoke",
+             "Resource": "execute-api:/${var.rest_api_stage_name}/*/*",
+             "Condition": {
+                 "StringNotEquals": {
+                     "aws:sourceVpc": "${var.vpc_id}"
+                 }
+             }
+         },
+         {
+             "Effect": "Allow",
+             "Principal": "*",
+             "Action": "execute-api:Invoke",
+             "Resource": "execute-api:/${var.rest_api_stage_name}/*/*"
+         }
+     ]
+ }
+ EOF
+     vpc_endpoint_ids        = [data.aws_vpc_endpoint.endpoint.id]
+     stage_name              = var.rest_api_stage_name
+     stage_description       = var.stage_description 
+     path_part               = var.path_part
+     method                  = var.http_method
+     authorization           = var.authorization
+     lambda_arn              = module.lambda_s3.arn
+     region                  = var.region
+     account_id              = var.account_id
+     credentials             = module.role_lambda_s3.arn
+ }
--- terraform/data.tf
+++ terraform/data.tf
@@ -2,14 +2,14 @@ data "template_file" "buckets" {
   template = file("scripts/buckets.tpl")
 }
 
-# data "aws_security_group" "endpoint" {
-#   name = var.sg_name
-# }
+data "aws_security_group" "endpoint" {
+  name = var.sg_name
+}
 
-# data "aws_vpc_endpoint" "endpoint" {
-#   service_name = var.service_name
-# }
+data "aws_vpc_endpoint" "endpoint" {
+  service_name = var.service_name
+}
 
-# # data "aws_api_gateway_rest_api" "gateway" {
-# #   name = "${var.name}" 
-# # }
+# data "aws_api_gateway_rest_api" "gateway" {
+#   name = "${var.name}" 
+# }
